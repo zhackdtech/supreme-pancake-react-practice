82ed43e608bdd3caddfb655cc1f3f52017858235
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2023-09-10 21:46:17 +0800

    Added my hello-world

---
+++ hello-world/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from "react";
+function App() {
+  return React.createElement("h1",{
+    className: "my-h1",
+    style: {
+      color: "red"
+    },
+  },
+  "Hello world")
+}
+
+export default App;
